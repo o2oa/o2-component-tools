beaa11379c63feaa777bf99d061a56fffe9c7bc5
huqi <hq_1980@hotmail.com>
2021-10-15 19:59:28 +0800

    init

--- bin/o2.js
+++ bin/o2.js
@@ -2,17 +2,33 @@
 
 import { Command } from 'commander/esm.mjs';
 import fs from 'fs/promises';
+import create from '../lib/create.js';
 // import {cmd_addServer, cmd_delServer} from './commands/env.js'
 // import {cmd_getApplication} from './commands/application.js'
 const program = new Command();
 
 let pkg = JSON.parse(await fs.readFile('./package.json', {'encoding': 'utf8'}));
-program.version(pkg.version, '-v, --vers', 'output the current version')
-    .command('create' )
-    // .argument('<name>', 'Provide a name for the server configuration')
-    .description('Create a O2OA component')
-    .option('-n --name <value>', 'O2OA component name')
-    .action((opts) => { cmd_addServer(opts); });
+
+program.version(pkg.version, '-v, --vers', 'output the current version');
+
+program
+    .command('create <app-name>' )
+    .description('create a new O2OA component')
+    .option('-p, --preset <presetName>', 'Skip prompts and use saved or remote preset')
+    .option('-d, --default', 'Skip prompts and use default preset')
+    .option('-i, --inlinePreset <json>', 'Skip prompts and use inline JSON string as preset')
+    .option('-m, --packageManager <command>', 'Use specified npm client when installing dependencies')
+    .option('-r, --registry <url>', 'Use specified npm registry when installing dependencies (only for npm)')
+    .option('-g, --git [message]', 'Force git initialization with initial commit message')
+    .option('-n, --no-git', 'Skip git initialization')
+    .option('-f, --force', 'Overwrite target directory if it exists')
+    .option('--merge', 'Merge target directory if it exists')
+    .option('-c, --clone', 'Use git clone when fetching remote preset')
+    .option('-x, --proxy <proxyUrl>', 'Use specified proxy when creating project')
+    .option('-b, --bare', 'Scaffold project without beginner instructions')
+    .option('--skipGetStarted', 'Skip displaying "Get started" instructions')
+    .action((name, opts) => {
+        create(name, opts); });
 
 program .command('serve')
     .description('Start dev server')
