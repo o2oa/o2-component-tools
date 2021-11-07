b61abeaee874dc77202cfa4ec04ceb16836da016
huqi <hq_1980@hotmail.com>
2021-11-07 11:28:04 +0800

    modify o2-deploy

--- commands/create.js
+++ commands/create.js
@@ -53,7 +53,7 @@ class componentFactory{
 
             const pkgStr = await fs.readFile(path.resolve(componentPath, 'package.json'), 'utf8');
             const pkg = JSON.parse(pkgStr);
-            pkg.scripts['o2-deploy'] = 'cross-env BUILD_PATH=../../dest/'+componentPath+' react-scripts build';
+            pkg.scripts['o2-deploy'] = `cross-env BUILD_PATH=../../dest/${componentPath} PUBLIC_URL=../${componentPath}/ react-scripts build`;
 
             fs.writeFile(path.resolve(componentPath, 'package.json'), JSON.stringify(pkg, '\t'));
 
